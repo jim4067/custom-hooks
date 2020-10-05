6b831d0c9c1007a4e4a41527398afce7f6a39579
jim4067 <jimmyimpulse2@gmail.com>
2020-10-05 21:04:24 +0300

    feat: custom hook for using forms to add data

--- src/App.js
+++ src/App.js
@@ -1,35 +1,61 @@
 import React, { useState } from 'react';
 
-const useCounter = () =>  {
-    const [value, setValue] = useState(0);
+const useCounter = () => {
+	const [value, setValue] = useState(0);
 
-    const increase = ( ) => {
-        setValue(value + 1);
-    }
-    const decrease = () => {
-        setValue(value - 1);
-    }
-    const zero = () => {
-        setValue(0);
-    }
-    return{
-        value,
-        increase,
-        decrease,
-        zero
-    }
+	const increase = () => {
+		setValue(value + 1);
+	}
+	const decrease = () => {
+		setValue(value - 1);
+	}
+	const zero = () => {
+		setValue(0);
+	}
+	return {
+		value,
+		increase,
+		decrease,
+		zero
+	}
+}
+
+const useField = (type)  => {
+	const [value , setValue] = useState('');
+
+	const onChange = (event) => {
+		setValue(event.target.value);
+	}
+
+	return{
+		onChange,
+		type,
+		value
+	}
 }
 
 const App = () => {
-  const counter = useCounter();
+	const born = useField('date');
+	const height = useField('number');
+	const name = useField('text');
 
-  return(
-    <div>
-      <div>{counter.value} </div>
-      <button onClick={counter.increase}>increase</button>
-      <button onClick={counter.decrease}>decrease</button>
-      <button onClick={counter.zero}>zero</button>
-    </div>
-  );
+	return (
+		<div>
+
+			<form>
+				name <input {...name} />
+				<br />
+				height <input {...height} />
+				<br />
+				born <input {...born} />
+				<br />
+			</form>
+
+			<div> 
+				{name.value} {height.value} {born.value}
+			</div>
+
+		</div>
+	);
 }
 export default App;
